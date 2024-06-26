e07d35325d91408a57fac9dcd44e140783720102
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-06-26 13:55:02 +0200

    fix

--- archinstall.sh
+++ archinstall.sh
@@ -20,11 +20,10 @@ setEnvironmentVariables() {
     echo $INSTALL_LOG
     set +a
 
-    if [ ! -d $BASE_DIR ]; then return 1; fi
     if [ ! -d $SCRIPTS_DIR ]; then return 2; fi
     if [ ! -d $CONFIGS_DIR ]; then return 3; fi
     if [ ! -d $ASSETS_DIR ]; then return 4; fi
-    #if [ ! -d $LOGS_DIR ]; then return 5; fi
+    if [ ! -d $LOGS_DIR ]; then return 5; fi
 
     return 0
 }
@@ -40,7 +39,7 @@ if [ $? -eq 0 ]; then
     ( bash ${SCRIPTS_DIR}/0.preinstall.sh )
 
     # esecuzione dello script di installazione
-    ( arch-chroot /mnt ${HOME}/archinstall/scripts/1.core.sh )
+    #( arch-chroot /mnt ${HOME}/archinstall/scripts/1.core.sh )
 
     # esecuzione dello script di post-installazione
     #( arch-chroot /mnt ${HOME}/archinstall/scripts/2.finalization.sh )
