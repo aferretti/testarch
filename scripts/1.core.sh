240b002bdd75b713180abfc54aaf896bcb7305f0
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-06-27 13:53:23 +0200

    fix

--- scripts/1.core.sh
+++ scripts/1.core.sh
@@ -15,7 +15,7 @@ setUsers() {
 
     alreadyExists=( grep '${USER}' /etc/passwd )
     #if [ $alreadyExists -eq 1 ]; then
-    if grep "fertec:" /etc/passwd; then
+    if -z grep "fertec:" /etc/passwd; then
         echo "ci sono"
         exit
         useradd -m -g users -G wheel ${USER}
