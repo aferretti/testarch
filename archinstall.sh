e02aa232c474b4ddb7288bc794309b012db7171b
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-06-27 12:36:08 +0200

    fix

--- archinstall.sh
+++ archinstall.sh
@@ -66,13 +66,13 @@ if [ $? -eq 0 ]; then
     setParameters
 
     # esecuzione dello script di pre-installazione
-    #( bash ${SCRIPTS_DIR}/0.preinstall.sh )
+    ( bash ${SCRIPTS_DIR}/0.preinstall.sh )
 
     # esecuzione dello script di installazione
-    #( arch-chroot /mnt ${HOME}/archinstall/scripts/1.core.sh )
+    ( arch-chroot /mnt ${HOME}/archinstall/scripts/1.core.sh )
 
     # esecuzione dello script di post-installazione
-    #( arch-chroot /mnt ${HOME}/archinstall/scripts/2.finalization.sh )
+    ( arch-chroot /mnt ${HOME}/archinstall/scripts/2.finalization.sh )
 else
     printf "HELP %d" $?
 fi
--- scripts/2.finalization.sh
+++ scripts/2.finalization.sh
@@ -11,18 +11,22 @@ source ${SCRIPTS_DIR}/helpers.sh
 
 installOpenbox() {
     homePath="/home/${USER}"
+    xinitrcFile = "${homePath}/.xinitrc"
 
     pacman -S openbox xorg-server xorg-xinit xorg-fonts-misc xterm --noconfirm --needed
     checkError "pacman -S openbox xorg-server xorg-xinit xorg-fonts-misc xterm --noconfirm --needed"
 
-    cp "/etc/X11/xinit/xinitrc" "${homePath}/.xinitrc"
-    checkError "cp \"/etc/X11/xinit/xinitrc\" \"${homePath}/.xinitrc\""
+    cp "/etc/X11/xinit/xinitrc" "${xinitrcFile}"
+    checkError 'cp "/etc/X11/xinit/xinitrc" "${xinitrcFile}"'
+
+    grep -qxF 'exec openbox-session' ${xinitrcFile} || echo 'exec openbox-session' >> ${xinitrcFile}
+    checkError "grep -qxF 'exec openbox-session' ${xinitrcFile} || echo 'exec openbox-session' >> ${xinitrcFile}"
 
     mkdir -p "${homePath}/.config/openbox"
-    checkError "mkdir -p \"${homePath}/.config/openbox\""
+    checkError 'mkdir -p "${homePath}/.config/openbox"'
 
     cp -a "/etc/xdg/openbox/" "${homePath}/.config/"
-    checkError "cp -a \"/etc/xdg/openbox/\" \"${homePath}/.config/\""
+    checkError 'cp -a "/etc/xdg/openbox/" "${homePath}/.config/"'
 }
 
 setHostname() {
@@ -36,32 +40,35 @@ setHostname() {
     touch ${hostnameFile}
     checkError "touch ${hostnameFile}"
 
-    echo ${HOST} >> ${hostnameFile}
+    echo ${DEVID} >> ${hostnameFile}
 }
 
 cleanup() {
     rm -r "${HOME}/ArchInstall"
-    checkError "rm -r ${HOME}/ArchInstall"
+    checkError 'rm -r "${HOME}/ArchInstall"'
 }
 
 prepareUserScripts() {
     scriptsPath="/home/${USER}/startup"
     configFile="${scriptsPath}/env.conf"
 
-    if [ ! -d $scriptsPath ]; then 
-        mkdir $scriptsPath
-        checkError "mkdir $scriptsPath"
+    if [ ! -d ${scriptsPath} ]; then 
+        mkdir "${scriptsPath}"
+        checkError 'mkdir "${scriptsPath}"'
     fi
 
-    if [ ! -f $configFile ]; then 
-        touch $configFile
-        checkError "touch $configFile"
+    if [ ! -f ${configFile} ]; then 
+        touch "${configFile}"
+        checkError 'touch "${configFile}"'
     fi
 
-    echo "PROJECT_NAME=${}" >> ${configFile}    
-    echo "WORKSPACE_FOLDER=~/workspace/${}" >> ${configFile}
+    echo "PROJECT_NAME=${APP,}" >> ${configFile}    
+    echo "WORKSPACE_FOLDER=~/workspace/${APP,,}" >> ${configFile}
     echo "TTY_SYMLINK_ALIAS=${}" >> ${configFile}
     echo "USE_DOCKER=${}" >> ${configFile}
+
+    cp "${SCRIPTS_DIR}/4.app.sh" "${scriptsPath}/"
+    checkError 'cp "${SCRIPTS_DIR}/4.app.sh" "${scriptsPath}/"'
 
     #TODO: mettere in bash.rc l'esecuzione dello startup
 }
@@ -77,7 +84,7 @@ umountAndReboot() {
 
 # 
 clear
-showHeader "finalization"
+showHeader "Setup finalization"
 
 installOpenbox
 setHostname
