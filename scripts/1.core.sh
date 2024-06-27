3f83ed44705a70c244b7a435c70b701908af4234
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-06-27 16:33:54 +0200

    fix

--- scripts/1.core.sh
+++ scripts/1.core.sh
@@ -128,11 +128,11 @@ setAutologin() {
 # Inizializzazione degli utenti e delle password e installazione packages sistema operativo e abilitazione servizi
 clear
 showHeader "Users setup and base/linux packages installation"
-: '
+
 setUsers
 installBasePackages
 installLinuxPackages
-'
+
 # Impostazioni timezone, locale e preparazione GRUB
 clear
 showHeader "Date/Time, Locales and Grub setup"
@@ -145,6 +145,6 @@ clear
 showHeader "Default editor and autologin setup"
 
 #setDefaultEditor
-setAutologin
+#setAutologin
 
 waitForInput
