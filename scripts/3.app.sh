b4c93af0b0565b3e6c4f722b8f94a5578dbd5f3d
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-07-24 17:54:53 +0200

    Fix

--- scripts/3.app.sh
+++ scripts/3.app.sh
@@ -4,42 +4,8 @@
 source ${HOME}/startup/env.conf
 
 waitForInput() {
-    printf "Premere un tasto per continuare..."
+    printf "Premere un tasto per cleanup e riavvio..."
     read -n 1 -s
-}
-
-setConnectionName() {
-    ethName="LAN"
-
-    sourceEthName=$(nmcli -g name connection show | head -1)
-
-    if [ ! -z "${sourceEthName}" ]; then
-        sudo nmcli connection modify "${sourceEthName}" con-name "${ethName}"
-    else 
-        return 1
-    fi
-}
-
-setIpAddress() {
-    if [ ! -z $IP ]; then
-        if [ -z $GTW ]; then GTW="192.168.3.1"; fi
-
-        echo "setConnectionName"
-        waitForInput
-
-        setConnectionName
-
-        if [ ! -z ${ethName} ]; then
-            echo "nmcli ip"
-            waitForInput
-            sudo nmcli con mod ${ethName} ipv4.addresses ${IP}/24 ipv4.gateway ${GTW} ipv4.dns 8.8.8.8 ipv4.method manual
-
-            echo "systemctl restart"
-            waitForInput
-            sudo systemctl restart NetworkManager.service
-        fi
-        exit
-    fi
 }
 
 cleanupAndReboot() {
@@ -52,12 +18,10 @@ cleanupAndReboot() {
     sudo reboot
 }
 
-# Impostazione indirizzo IP statico
-setIpAddress
-
 #####################################
 # avenneri: qui...
 #####################################
 
 # Pulizia e riavvio
-#cleanupAndReboot
+waitForInput
+cleanupAndReboot
