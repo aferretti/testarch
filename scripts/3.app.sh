bb9f8d39f775a0f1373cf771bef18287acc13b8b
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-09-18 17:00:02 +0200

    Fix vari

--- scripts/3.app.sh
+++ scripts/3.app.sh
@@ -20,7 +20,7 @@ saveLogAndExit() {
 
 getEthName() {
     ETHNAME="eu-lan"    
-    echo ${PASSWORD} | sudo -S nmcli connection modify Wired\ connection\ 1 con-name ${ETHNAME}
+    echo ${PASSWORD} | sudo -S nmcli connection modify Wired\ connection\ 1 con-name ${ETHNAME} >> /dev/null
 }
 
 setIpAddress() {
@@ -28,22 +28,21 @@ setIpAddress() {
         getEthName
         if [ -z ${ETHNAME} ]; then saveLogAndExit "ERROR! No active ethernet interface found"; fi
 
-        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} ipv4.addresses "${IP}/24" ipv4.gateway "${GTW}"
-        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} ipv4.method manual
-        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} connection.autoconnect yes
-        #echo ${PASSWORD} | sudo -S nmcli connection up ${ETHNAME}
+        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} ipv4.addresses "${IP}/24" ipv4.gateway "${GTW}" >> /dev/null
+        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} ipv4.method manual >> /dev/null
+        echo ${PASSWORD} | sudo -S nmcli connection modify ${ETHNAME} connection.autoconnect yes >> /dev/null
     fi
 }
 
 cleanupAndReboot() {
     # rimozione della riga che esegue lo startup dal file .bashrc
-    sed -i '/source /home/fertec/startup/3\.app\.sh/d' ${HOME}/.bashrc
+    #sed -i '/source /home/fertec/startup/3\.app\.sh/d' ${HOME}/.bashrc
 
     # rimozione della cartella contenente gli script di avvio
     if [ -d ${HOME}/startup ]; then rm -r ${HOME}/startup; fi
 
     # riavvio
-    echo ${PASSWORD} | sudo -S reboot
+    echo ${PASSWORD} | sudo -S reboot >> /dev/null
 }
 
 #####################################
@@ -52,6 +51,7 @@ cleanupAndReboot() {
 
 # Impostazione indirizzo IP statico
 setIpAddress
+waitForInput 
 
 # Pulizia e riavvio
 cleanupAndReboot
