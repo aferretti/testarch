d69bbc34976269b925776485686130f938188285
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-09-18 14:34:04 +0200

    Fix vari

--- scripts/3.app.sh
+++ scripts/3.app.sh
@@ -9,7 +9,7 @@ waitForInput() {
 }
 
 getEthName() {
-    ETHNAME="Wired\ connection\ 1"
+    ETHNAME='Wired connection 1'
 
     # for name in $(nmcli -t -f NAME c show --active) ; do
     #     ETHNAME=${name}
@@ -26,7 +26,7 @@ setIpAddress() {
             exit
         fi
 
-        echo f3rt3c | sudo -S nmcli connection modify '${ETHNAME}' con-name culo
+        echo f3rt3c | sudo -S nmcli connection modify "${ETHNAME}" con-name culo
     fi
 }
 
