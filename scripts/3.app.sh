7874c5d41ed63043dbf561f1c0b4b255decb20ba
Adriano Ferretti <adri@Adri-MacBook-Pro.local>
2024-09-18 17:22:54 +0200

    Fix vari

--- scripts/3.app.sh
+++ scripts/3.app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Caricamento file env.conf
-source ${HOME}/startup/env.conf
+#source ${HOME}/startup/env.conf
 
 waitForInput() {
     printf "Premere un tasto per cleanup e riavvio..."
@@ -36,13 +36,19 @@ setIpAddress() {
 
 cleanupAndReboot() {
     # rimozione della riga che esegue lo startup dal file .bashrc
+    bashFile=${HOME}/.bashrc
+
+    if [ -f $[bashFile] ]; then
+        grep -v "source /home/fertec/startup/3.app.sh" ${bashFile} > ${bashFile}2 
+        mv ${bashFile}2 ${bashFile}
+    fi
     #sed -i '/source /home/fertec/startup/3\.app\.sh/d' ${HOME}/.bashrc
 
     # rimozione della cartella contenente gli script di avvio
-    if [ -d ${HOME}/startup ]; then rm -r ${HOME}/startup; fi
+    #if [ -d ${HOME}/startup ]; then rm -r ${HOME}/startup; fi
 
     # riavvio
-    echo ${PASSWORD} | sudo -S reboot >> /dev/null
+    #echo ${PASSWORD} | sudo -S reboot >> /dev/null
 }
 
 #####################################
@@ -50,8 +56,8 @@ cleanupAndReboot() {
 #####################################
 
 # Impostazione indirizzo IP statico
-clear
-setIpAddress
+#setIpAddress
+#clear
 
 # Pulizia e riavvio
 cleanupAndReboot
